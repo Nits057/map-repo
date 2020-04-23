a28277bc2c50f0996ac2ad3ee33e556424519be0
G0dlikecoder <55620900+G0dlikecoder@users.noreply.github.com>
2020-04-24 03:21:25 +0530

    first commit

---
+++ components/HelloWorld3.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello" style="height: 10vh; width: 40vw;">
+    
+  <p >{{ post }}</p>
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld',
+  props: ['post'],
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/* .image2{
+  height: 60px;
+  width: 80px;
+} */
+
+</style>
